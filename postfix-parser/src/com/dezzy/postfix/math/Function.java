716ff65f275bc31c7a0361426e60a678a21d63c8
dezzy <dezzy@DESKTOP-A3T1TV3.bc.edu>
2020-02-09 00:52:55 -0500

    added documentation

--- postfix-parser/src/com/dezzy/postfix/math/Function.java
+++ postfix-parser/src/com/dezzy/postfix/math/Function.java
@@ -16,28 +16,64 @@ public interface Function {
 	 */
 	public double apply(final double x);
 	
+	/**
+	 * @see Math#sin(double)
+	 */
 	public static final Function sin = Math::sin;
 	
+	/**
+	 * @see Math#cos(double)
+	 */
 	public static final Function cos = Math::cos;
 	
+	/**
+	 * @see Math#tan(double)
+	 */
 	public static final Function tan = Math::tan;
 	
+	/**
+	 * @see Math#asin(double)
+	 */
 	public static final Function invsin = Math::asin;
 	
+	/**
+	 * @see Math#acos(double)
+	 */
 	public static final Function invcos = Math::acos;
 	
+	/**
+	 * @see Math#atan(double)
+	 */
 	public static final Function invtan = Math::atan;
 	
+	/**
+	 * @see Math#sinh(double)
+	 */
 	public static final Function sinh = Math::sinh;
 	
+	/**
+	 * @see Math#cosh(double)
+	 */
 	public static final Function cosh = Math::cosh;
 	
+	/**
+	 * @see Math#tanh(double)
+	 */
 	public static final Function tanh = Math::tanh;
 	
+	/**
+	 * @see Math#log(double)
+	 */
 	public static final Function ln = Math::log;
 	
+	/**
+	 * @see Math#log10(double)
+	 */
 	public static final Function log10 = Math::log10;
 	
+	/**
+	 * @see Math#abs(double)
+	 */
 	public static final Function abs = Math::abs;
 	
 	
